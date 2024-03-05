db506eabe3a7c651e1e48d0db8a76b6476aac59f
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-05 14:29:12 +0200

    Created e AllexpenseViewModel and FilterdExpemsesViewModel

---
+++ Billing_System.Core/ViewModels/Expense/AllExpenseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Billing_System.Core.ViewModels.Expense
+{
+    public class AllExpenseViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Value { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public string ReceiptUrl { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+    }
+}
---
+++ Billing_System.Core/ViewModels/Expense/FilteredExpensesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Billing_System.Core.ViewModels.Expense
+{
+    public class FilteredExpensesViewModel
+    {
+        public string? Filter { get; set; }
+        public string? OrderBy { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int ExpensesCount { get; set; }
+        public ICollection<AllExpenseViewModel> Expenses { get; set; } = new List<AllExpenseViewModel>();
+    }
+}
