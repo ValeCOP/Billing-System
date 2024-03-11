1207367b3f5228c76929546253f2a1e4370b159f
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-11 15:58:57 +0200

    Removed unusable method

--- Billing_System.Core/Contracts/Clients/IClientsService.cs
+++ Billing_System.Core/Contracts/Clients/IClientsService.cs
@@ -10,6 +10,6 @@
         Task<ICollection<ActivatedClientsViewModel>> GetAllClientsAsync(string orderBy, string searchString);
         Task<ActivatedClientsViewModel> GetClientDetailsAsync(Guid id);
         Task<ICollection<ShortInfoForActivatedClients>> GetClientShortAsync();
-        Task<int> GetTotalPages(int pageSize);
+       
     }
 }
