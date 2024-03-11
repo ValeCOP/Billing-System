03fc5e9eba42ef68b8c61c539b90a36d9a8a995a
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-11 15:57:15 +0200

    Added functionality for creted automatic bonus payment

--- Billing_System/Controllers/Promotions/PromotionController.cs
+++ Billing_System/Controllers/Promotions/PromotionController.cs
@@ -1,28 +1,51 @@
 ﻿namespace Billing_System.Controllers.Promotions
 {
+    using Billing_System.Core.Contracts.Home;
+    using Billing_System.Core.Contracts.Payments;
     using Billing_System.Core.Contracts.Promotion;
-    using Billing_System.Data.Entities;
+    using Billing_System.Core.CustomExtensions;
+    using Billing_System.Core.ViewModels.Payments;
     using Billing_System.ViewModels;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using System.Diagnostics;
-    using System.Globalization;
 
     public class PromotionController : Controller
     {
-        private readonly IPromotionServise _promotionService;
+        private readonly IPromotionService _promotionService;
+        private readonly IPaymentsService _paymentsService;
+        private readonly IHomeService _homeService;
 
-        public PromotionController(IPromotionServise promotionService)
+        public PromotionController(IPromotionService promotionService, IPaymentsService paymentsService,IHomeService homeService)
         {
+            _homeService = homeService;
+            _paymentsService = paymentsService;
             _promotionService = promotionService;
         }
 
         [HttpPost]
-        public IActionResult Add(Guid clientId)
+        public async Task<IActionResult> Add(Guid clientId)
         {
             try
             {
-                _promotionService.Add(clientId);
+                await _promotionService.Add(clientId);
+
+                AddPaymentViewModel payment = new AddPaymentViewModel
+                {
+                    Name = "Promotion",
+                    Fee = 0,
+                    Pending = false,
+                    Receipt = false,
+                    FromDate = DateTime.Now.ToString(),
+                    ToDate = DateTime.Now.AddMonths(2).ToString(),
+                    ClId = clientId,
+                    UserId = Guid.Parse(User.GetId())
+                };
+
+                await _paymentsService.AddPaymentAsync(payment, payment.UserId);
+
+                await _homeService.UpdateISPRouterDataAsync(clientId);
+
+                return Ok(200);
             }
             catch (Exception ex)
             {
@@ -31,8 +54,7 @@
                     RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
                     Message = ex.Message
                 });
-            }   
-            return Ok();
-        }   
+            }
+        }
     }
 }
