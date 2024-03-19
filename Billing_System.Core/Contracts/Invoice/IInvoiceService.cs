d6eca4c53ce5b39d8d28104d93e2d0724a7662da
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-19 18:00:42 +0200

    Added CreateInvoiceAsync method

--- Billing_System.Core/Contracts/Invoice/IInvoiceService.cs
+++ Billing_System.Core/Contracts/Invoice/IInvoiceService.cs
@@ -1,7 +1,11 @@
 ﻿namespace Billing_System.Core.Contracts.Invoice
 {
+    using Billing_System.Core.ViewModels.Invoice;
+    using System.Threading.Tasks;
+
     public interface IInvoiceService
     {
+        Task CreateInvoiceAsync(CreateInvoiceViewModel model, Guid paymentId);
         int GetNextInvoiceNumber();
     }
 }
--- Billing_System.Core/Services/Invoice/InvoiceService.cs
+++ Billing_System.Core/Services/Invoice/InvoiceService.cs
@@ -1,8 +1,10 @@
 ﻿namespace Billing_System.Core.Services.Invoice
 {
     using Billing_System.Core.Contracts.Invoice;
+    using Billing_System.Core.ViewModels.Invoice;
     using Billing_System.Data;
-
+    using System.Threading.Tasks;
+    using Billing_System.Data.Entities;
     public class InvoiceService : IInvoiceService
     {
         private readonly BillingDbContext _context;
@@ -11,9 +13,26 @@
         {
             _context = context;
         }
+
+        public async Task CreateInvoiceAsync(CreateInvoiceViewModel model, Guid paymentId)
+        {
+            var invoice = new Invoice
+            {
+                MOL = model.MOL,
+                UIN = model.UIN,
+                VATIN = model.VATIN,
+                Recipient = model.Recipient,
+                Compiler = model.Compiler,
+                PaymentId = paymentId,
+                CreatedOn = DateTime.Now
+            };
+            await _context.Invoices.AddAsync(invoice);
+            await _context.SaveChangesAsync();
+        }
+
         public int GetNextInvoiceNumber()
         {
-            return _context.Invoices.Count() + 1;
+            return _context.Invoices.Count() + 49;
         }
     }
 }
