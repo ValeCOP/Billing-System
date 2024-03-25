1e29959552a38f750c36ee5d2358898eeeb8e18d
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-25 15:22:12 +0200

    Changed property InvoiceNumber on string

---
+++ Billing_System.data/Migrations/20240324131400_1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Billing_System.Data.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
