d7f85b597bf15d2af5082c2eedb29fb1cb0c83f8
Vale_COP@students.softuni.bg <Vale_COP@students.softuni.bg>
2024-03-29 12:23:33 +0200

    Implemented viewComponents

---
+++ Billing_System/ViewComponents/BankDetailsViewComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Billing_System.ViewComponents
+{
+    using Billing_System.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BankDetailsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Company company = new Company();
+            return View(company);
+        }
+    }
+}
---
+++ Billing_System/ViewModels/Company.cs
@@ -0,0 +1,14 @@
+﻿namespace Billing_System.ViewModels
+{
+    public class Company
+    {
+        public string Name { get; set; } = "Инфокаст системс";
+        public string Address { get; set; } = "София, Саmоков, с.Райово, 2017";
+        public string EIK { get; set; } = "203231847";
+        public string VAT { get; set; } = string.Empty;
+        public string Manager { get; set; } = "Снежана Г. Никова";
+         public string Account { get; set; } = "BG08UNCR70001522871893";
+        public string Bank { get; set; } = "Булбанк";
+        public string BIC { get; set; } = "BUIBGSF";
+    }
+}
